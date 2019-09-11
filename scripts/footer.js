1988f096dcf74e2f48958a261804419f711b2b3e
Hector Robles <hectorlearns2code@gmail.com>
2019-09-11 16:14:11 +0800

    add fade in

---
+++ scripts/footer.js
@@ -0,0 +1,24 @@
+sr.reveal('.reveal--left',{
+    origin: 'left',
+    distance: "50px",
+    reset: false,
+    duration: 1000,
+    viewFactor: .5
+});
+
+sr.reveal('.reveal--right',{
+    origin: 'right',
+    distance: "25px",
+    reset: false,
+    duration: 1000,
+    viewFactor: .5
+});
+
+
+sr.reveal('.reveal--bottom',{
+    origin: 'bottom',
+    distance: "50px",
+    reset: false,
+    duration: 1000,
+    viewFactor: .5
+});
---
+++ scripts/header.js
@@ -0,0 +1,8 @@
+window.sr = ScrollReveal({
+    distance: '50px',
+    duration: 1000,
+    easing: 'ease',
+    mobile: true,
+    reset: true,
+    viewFactor: 0.4,
+  });
